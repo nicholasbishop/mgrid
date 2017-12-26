f4f678f7cbddcb3a243188406f5e446130740ab1
Nicholas Bishop <nicholasbishop@gmail.com>
2017-12-26 13:50:05 -0500

    Code cleanup

--- src/shaders/grid_tess_ctrl.glsl
+++ src/shaders/grid_tess_ctrl.glsl
@@ -3,18 +3,21 @@
 layout(vertices = 4) out;
 in vec3 vPosition[];
 out vec3 tcPosition[];
+
+const int inner = 3;
+const int outer = 4;
 	 
 #define ID gl_InvocationID
 	 
 void main()
 {
   tcPosition[ID] = vPosition[ID];
-  //if (ID == 0) {
-    gl_TessLevelInner[0] = 4;
-    gl_TessLevelInner[1] = 4;
-    gl_TessLevelOuter[0] = 6;
-    gl_TessLevelOuter[1] = 6;
-    gl_TessLevelOuter[2] = 6;
-    gl_TessLevelOuter[3] = 6;
-    //}
+  if (ID == 0) {
+    gl_TessLevelInner[0] = inner;
+    gl_TessLevelInner[1] = inner;
+    gl_TessLevelOuter[0] = outer;
+    gl_TessLevelOuter[1] = outer;
+    gl_TessLevelOuter[2] = outer;
+    gl_TessLevelOuter[3] = outer;
+  }
 }
