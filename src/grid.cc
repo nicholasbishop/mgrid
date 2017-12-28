ad2e47c92ee207140cb5d8e71a3e51e57c7f9d51
Nicholas Bishop <nicholasbishop@gmail.com>
2017-12-28 12:59:26 -0500

    Use new grid iter in intersect method

--- src/grid.cc
+++ src/grid.cc
@@ -34,39 +34,22 @@ void Grid::make_random(const ivec2& res) {
 }
 
 optional<vec4> Grid::intersect_ray(const Ray3& ray) {
-  const int w = res_.x;
-  const int h = res_.y;
-
   optional<vec4> hit;
 
-  for (int y = 1; y < h; y++) {
-    for (int x = 1; x < w; x++) {
-      const int ind[4] = {
-          // clang-format off
-          (y - 1) * w + (x - 1),
-          (y - 1) * w + (x - 0),
-          (y - 0) * w + (x - 0),
-          (y - 0) * w + (x - 1),
-          // clang-format on
-      };
-      const int pattern[2][3] = {{0, 1, 2}, {0, 2, 3}};
-      for (int t = 0; t < 2; t++) {
-        Triangle tri{points_[ind[pattern[t][0]]], points_[ind[pattern[t][1]]],
-                     points_[ind[pattern[t][2]]]};
+  triangle_iter([this, &ray, &hit](const std::array<std::size_t, 3>& ind) {
+    Triangle tri{points_[ind[0]], points_[ind[1]], points_[ind[2]]};
 
-        //   GLM_FUNC_DECL bool intersectRayTriangle(
-        // vec<3, T, Q> const& orig, vec<3, T, Q> const& dir,
-        // vec<3, T, Q> const& v0, vec<3, T, Q> const& v1, vec<3, T, Q> const&
-        // v2,
-        // vec<3, T, Q>& baryPosition, T& distance);
+    //   GLM_FUNC_DECL bool intersectRayTriangle(
+    // vec<3, T, Q> const& orig, vec<3, T, Q> const& dir,
+    // vec<3, T, Q> const& v0, vec<3, T, Q> const& v1, vec<3, T, Q> const&
+    // v2,
+    // vec<3, T, Q>& baryPosition, T& distance);
 
-        const auto res = intersect_ray_triangle(ray, tri);
-        if (res && hit && res->w < hit->w) {
-          hit = res;
-        }
-      }
+    const auto res = intersect_ray_triangle(ray, tri);
+    if (res && hit && res->w < hit->w) {
+      hit = res;
     }
-  }
+  });
 
   return hit;
 }
